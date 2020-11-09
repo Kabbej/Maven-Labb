5fb523fbb3b2c0b5a4e3c8b26a979fbedd637576
Kabbej <kabbe.jartun@gmail.com>
2020-11-09 13:36:12 +0100

    initial commit

---
+++ src/main/java/TextProcessor.java
@@ -0,0 +1,14 @@
+public class TextProcessor {
+
+    public String upperCase (){
+        return null;
+    }
+
+    public String lowerCase (){
+        return null;
+    }
+
+    public String reverse (){
+        return null;
+    }
+}
