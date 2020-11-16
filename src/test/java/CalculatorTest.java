61ad0449d06ed575919bc1e03dd7efa5015c6460
Kabbej <kabbe.jartun@gmail.com>
2020-11-16 13:16:58 +0100

    Added more test methods

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,20 +1,40 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.runners.Parameterized;
+import org.w3c.dom.ls.LSOutput;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
+    Calculator calculator = new Calculator();
+
+    @BeforeEach
+    void beforeEach (){
+        System.out.println("Körs före varje test");
+    }
 
     @Test
     void testAdd (){
-        Calculator calculator = new Calculator();
         assertEquals(4,calculator.add(2,2));
     }
 
     @Test
     @DisplayName("Testar multiply-metoden")
     void testMultiply (){
-        Calculator calculator = new Calculator();
         assertNotEquals(0,calculator.multiply(2,5));
     }
+
+    @Test
+    void testDivide (){
+        assertNotNull(calculator.divide(4,2));
+    }
+
+    @Test
+    @Disabled
+    void testSubtract (){
+        assertEquals(0,calculator.subtract(2,2));
+    }
+
 }
---
+++ src/test/java/TextProcessorTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TextProcessorTest {
+
+    @Test
+    void upperCase() {
+    }
+
+    @Test
+    void lowerCase() {
+    }
+
+    @Test
+    void reverse() {
+    }
+}
